26b033e72c9797ee9a7500693b1c5067f93dd52b
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-05-16 23:47:05 +0200

    javascript fixed

--- js/main.js
+++ js/main.js
@@ -1,34 +1,46 @@
-function validContactData() {
+function validContactName() {
 	var name = document.getElementById('name');
-	var email = document.getElementById('email');
-	var message = document.getElementById('message');
-	var submit_button = document.getElementById('submit_button');
-	
 	var nameLength = name.value.length;
-	var lastThreeCharactersOfEmail = id.substr(id.length - 3);
-	var lastFourCharactersOfEmail = id.substr(id.length - 4);
-	var messageLength = message.value.length;
 	
-	submit_button.disabled = true;
-	
-	if(!((nameLength>3)&&(nameLength<=30)&&(name.value.indexOf(" ")==-1)))
+	if(!((nameLength>3)&&(nameLength<=30)&&(name.value.indexOf(" ")!=-1)))
 	{
 		name.style.backgroundColor = "#FF0000";
+		document.getElementById("submit_button").disabled = true;
 	}
-	
-	else if(!((lastThreeCharactersOfEmail==".hu")||(lastFourCharactersOfEmail==".com")))
-	{
-		email.style.backgroundColor = "#FF0000";
-	}
-	
-	else if(!((messageLength>20)&&(nameLength<=500)))
-	{
-		name.style.backgroundColor = "#FF0000";
-	}
-	
 	else
 	{
-		submit_button.disabled = false;
+		name.style.backgroundColor = "#FFFFFF";
+	}
+}
+
+function validContactEmail() {
+	var email = document.getElementById('email');
+	var lastThreeCharactersOfEmail = email.value.substr(email.value.length - 3);
+	var lastFourCharactersOfEmail = email.value.substr(email.value.length - 4);
+	
+	if(!((lastThreeCharactersOfEmail==".hu")||(lastFourCharactersOfEmail==".com")))
+	{
+		email.style.backgroundColor = "#FF0000";
+		document.getElementById("submit_button").disabled = true;
+	}
+	else
+	{
+		email.style.backgroundColor = "#FFFFFF";
+	}
+}
+
+function validContactMessage() {
+	var message = document.getElementById('message');
+	var messageLength = message.value.length;
+	
+	if(!((messageLength>20)&&(messageLength<=500)))
+	{
+		message.style.backgroundColor = "#FF0000";
+		document.getElementById("submit_button").disabled = true;
+	}
+	else
+	{
+		message.style.backgroundColor = "#FFFFFF";
 	}
 }
 
@@ -38,5 +50,6 @@ function sendMessage()
 	var email = document.getElementById('email');
 	var message = document.getElementById('message');
 	
-	alert(name+" "+email+" "+message+" Üzeneted elküldve!");
+	alert(name.value+" "+email.value+" "+message.value+" Üzeneted elküldve!");
 }
+document.getElementById("submit_button").disabled = true;
